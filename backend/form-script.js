7255a3909ca74c0f2a93ac368ddd19d9db0967b9
adriiiaa12 <ap.poso1207@gmai.com>
2023-12-24 19:43:20 +0100

    first commit

---
+++ backend/form-script.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const webhookUrl = 'https://discord.com/api/webhooks/1188529503031537694/6xAhWvnfg7gN0SfWcPmFD-C02YE7tFEsFnZqcrSU5l23uxiemcRi4L8TTrZdkAdsI4sE';
+
+    const sendFormsButton = document.querySelector('.sendForms');
+    
+    sendFormsButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        const formData1 = new FormData(document.getElementById('appForm'));
+        const formData2 = new FormData(document.getElementById('appForm2'));
+
+        const message = formatFormData(formData1, formData2);
+
+        if (message) {
+            sendMessageToDiscord(message);
+        }
+    });
+
+    function formatFormData(formData1, formData2) {
+        
+        const message = `
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+**__Nueva solicitud__**
+> - __Nombre__: ${formData1.get('nombre')}
+> - __Método de contacto__: ${formData1.get('contacto')}
+> - __Plazo de entrega__: ${formData1.get('entrega')}
+> - __Tipo de aplicación__: ${formData1.get('compatibilidad')}
+> - __Propósito de la aplicación__: ${formData1.get('proposito')}
+> - __Funciones de la aplicación__: ${formData2.get('funciones')}
+> - __Diseño__: ${formData2.get('diseno')}
+> - __Comentarios__: ${formData2.get('comentarios')}
+> - __Idiomas__: ${formData2.get('idiomas')}
+> - __Algo más a añadir__: ${formData2.get('mas')}
+`;
+        
+        return message;
+    }
+
+    function sendMessageToDiscord(message) {
+        const payload = {
+            content: message,
+        };
+
+        fetch(webhookUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        })
+        .then((response) => {
+            if (response.ok) {
+                window.location.href = 'form-send-it.html';
+            } else {
+                alert('Error, no se ha enviado el formulario. Si persiste el error, ponte en contacto con adriiiaa_12#4859 en Discord.');
+            }
+        })
+        .catch((error) => {
+            alert('Error en la solicitud: ', error + ' Si persiste el error, ponte en contacto con adriiiaa_12#4859 en Discord.');
+        });
+    }
+});
--- backend/script.js
+++ backend/script.js
@@ -1,152 +1,142 @@
-document.addEventListener('DOMContentLoaded', function(){
-    if (!window.languageBlockExecuted) {
-        var language = navigator.language;
-    
-        if (language === 'es-ES' || language.startsWith('es')) {
-            spanishLanguageConfiguration();
-        } else if (language === 'en-EN' || language.startsWith('en')) {
-            englishLanguageConfiguration();
-        } else {
-            // alert('Other language : ' + language);
-            englishLanguageConfiguration();
-        }
-    
-        window.languageBlockExecuted = true;
-    }
-})
-
-anchoModelo = document.querySelector(".menu").offsetWidth;
-document.querySelector(".main-container").style.width = anchoModelo + 'px';
-
-var abierto = false;
-var altoFaq = document.querySelector(".faq-container").style.height;
-var textoFaq = document.querySelector(".preguntas-respuestas-faq");
-
-function encogerFaq() {
-    if (abierto) {
-        textoFaq.style.opacity = 0;
-        document.querySelector(".faq-container").style.height = '65px';
-    } else {
-        textoFaq.style.opacity = 1;
-        document.querySelector(".faq-container").style.height = altoFaq;
-    }
-
-    abierto = !abierto;
+function mobileVSpc() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 
-var originalState;
+var mobileSpan = document.querySelectorAll('.bubblesSpan');
 
-buyButtons = document.querySelectorAll(".buy-button-change-vs");
-buyButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        var allElements = document.querySelectorAll('body > *:not(.information-about-buying)');
-        var informationBuyDiv = document.querySelector('.information-about-buying');
-        informationBuyDiv.classList.remove('quitar-animacion');
-
-        if (!originalState) {
-            originalState = {
-                elements: [],
-                informationBuyDiv: informationBuyDiv.style.display
-            };
-    
-            allElements.forEach(function(element) {
-                originalState.elements.push({
-                    element: element,
-                    display: element.style.display
-                });
-            });
-        }
-    
-        allElements.forEach(function(element) {
-            element.classList.add('borroso');
-        });
-    
-        informationBuyDiv.style.display = 'block';
-    
-        document.querySelector(".restore-button").style.display = 'block';
+if (mobileVSpc()) {
+    mobileSpan.forEach(function(element) {
+        element.style.display = 'none';
     });
-})
-
-document.querySelector(".restore-button").addEventListener('click', function() {
-    var elementsToRestore = originalState.elements;
-    var informationBuyDiv = document.querySelector('.information-about-buying');
-
-    elementsToRestore.forEach(function(element) {
-        element.element.style.display = element.display;
+} else {
+    mobileSpan.forEach(function(element) {
+        element.style.display = 'block';
     });
+}
 
-    document.querySelectorAll('.borroso').forEach(function(element) {
-        element.classList.remove('borroso');
-    });
-    
-    informationBuyDiv.classList.add('quitar-animacion');
-    document.querySelector(".restore-button").style.display = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+    language = navigator.language;
 
-    function quitAnimationBuyDiv1(){
-        informationBuyDiv.style.display = originalState.informationBuyDiv;
+    if (language === 'es-ES' || language.startsWith('es')) {
+        alert('Bienvendo/a, gracias por entrar en nuestra página, esperamos que encuentres todo lo que estás buscando.');
+        window.scrollToHash();
+        spanishLanguageConfiguration()
+    } else if (language === 'en-EN' || language.startsWith('en')) {
+        alert('Welcome, thanks for entering in our web page, we hope you find all you are looking for.');
+        window.scrollToHash();
+        englishLanguageConfiguration()
+    } else {
+        alert('Welcome, thanks for entering in our web page, we hope you find all you are looking for.');
+        alert('Your language is not avaible in this page, so the language is in English, we hope you have any problems with the language. If you do not understant English, you can change your language in the footer page.')
+        window.scrollToHash();
+        englishLanguageConfiguration()
     }
-    setTimeout(quitAnimationBuyDiv1, 2500);
 });
 
-document.addEventListener('keydown', function(event) {
-    if(event.key === 'Escape') {
-        var elementsToRestore = originalState.elements;
-        var informationBuyDiv = document.querySelector('.information-about-buying');
-
-        elementsToRestore.forEach(function(element) {
-            element.element.style.display = element.display;
-        });
-
-        document.querySelectorAll('.borroso').forEach(function(element) {
-            element.classList.remove('borroso');
-        });
-        
-        informationBuyDiv.classList.add('quitar-animacion');
-        document.querySelector(".restore-button").style.display = 'none';
-
-        function quitAnimationBuyDiv(){
-            informationBuyDiv.style.display = originalState.informationBuyDiv;
-            safepassPanel.style.display = 'none';
-        }
-        setTimeout(quitAnimationBuyDiv, 2500);
-    }
-})
-
-function safepassShowPanel() {
-    safepassPanel = document.querySelector('.safepassPanel');
-    safepassPanel.style.display = 'block';
-}
-
 function spanishLanguageConfiguration() {
-    alert('Bienvenido/a, gracias por entrar en nuestra página, esperamos que encuentres todo lo que estás buscando.');
+    document.getElementsByClassName('header-buttons')[0].textContent = 'INICIO';
+    document.getElementsByClassName('header-buttons')[1].textContent = 'SOLICITAR APLICACIÓN';
+    document.getElementsByClassName('header-buttons')[2].textContent = 'SOPORTE';
+    document.getElementsByClassName('header-buttons')[3].textContent = 'FAQ';
 }
 
 function englishLanguageConfiguration() {
     document.getElementsByClassName('header-buttons')[0].textContent = 'HOME';
-    document.getElementsByClassName('header-buttons')[2].textContent = 'COMUNITY';
-    document.getElementsByClassName('header-buttons')[3].textContent = 'SUPPORT';
-
-    document.querySelectorAll('.buy-button-change-vs').textContent = 'Buy';
-
-    document.getElementById('supportTitle').textContent = 'SUPPORT';
-    document.getElementById('supportInput').ariaPlaceholder.textContent = 'Name';
-    document.getElementById('supportTextArea').ariaPlaceholder.textContent = 'Write your question...';
-
-    document.getElementById('discordTitle').textContent = 'Join our Discord server';
-    document.getElementById('discordP').textContent = 'In it questions about web page/bots are sent';
-    document.getElementById('discord-button').textContent = 'Join Now';
-
-    document.getElementById('titleFAQ').textContent = 'Frequently Asked Questions';
-    document.getElementById('que1').textContent = 'How can I get help?';
-    document.getElementById('que2').textContent = 'How can I download the bots that I want?';
-    document.getElementById('que3').textContent = 'Can you make a bot that I tell you?';
-    document.getElementById('ans1').textContent = 'You can get help sending a message in our Discord server or filling and sending the support form.';
-    document.getElementById('ans2').textContent = 'After making the payment (if you must pay for the bot), you must request help in the Discord server by opening a ticket with the "Download Bot" section.';
-    document.getElementById('ans3').textContent = 'You can send bots suggestion in our Discord server, opening a ticket in the "tickets" channel with "New Bot" section.';
-    
-    document.getElementById('copyright').textContent = '&copy; 2024 V4 Bots - All rights reserved';
-    alert('Welcome, thanks for entering in our web page, we hope you find all you are looking for.');
+    document.getElementsByClassName('header-buttons')[1].textContent = 'REQUEST APPLICATION';
+    document.getElementsByClassName('header-buttons')[2].textContent = 'SUPPORT';
+    document.getElementsByClassName('header-buttons')[3].textContent = 'FAQ';
 }
+
+// function quitNavBar() {
+//     if (mobileVSpc) {
+//         document.querySelector('.menu .navBar').style.display = 'none';
+//     } else {
+
+//     }
+// }
+
+// function showNavBar() {
+//     if (mobileVSpc) {
+//         document.querySelector('.menu .navBar').style.display = 'block';
+//     } else {
+
+//     }
+// }
+
+anchoModelo = document.querySelector(".menu").offsetWidth;
+document.querySelector(".main-container").style.width = anchoModelo + 'px';
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        this.parentElement.classList.toggle("active");
+
+        var pannel = this.nextElementSibling;
+
+        if (pannel.style.display === "block") {
+            pannel.style.display = "none";
+        } else {
+            pannel.style.display = "block";
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const webhookUrl = 'https://discord.com/api/webhooks/1188551161356242964/yrgHlVasJaKetb6NalC1QmcNWwtOaUxZLfJ0t4peepVaNQ0uMbUD_t6_kefEtMEPkv-q';
+
+    const supportForm = document.querySelector('.soporte form');
+    
+    supportForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        const formData = new FormData(supportForm);
+
+        const message = formatSupportFormData(formData);
+
+        if (message) {
+            sendMessageToDiscord(message);
+        }
+    });
+
+    function formatSupportFormData(formData) {
+        const message = `
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+**Formulario de Soporte**
+> - __Nombre__: ${formData.get('supportInput')}
+> - __Pregunta__: ${formData.get('supportTextArea')}
+        `;
+        
+        return message;
+    }
+
+    function sendMessageToDiscord(message) {
+        const payload = {
+            content: message,
+        };
+
+        fetch(webhookUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        })
+        .then((response) => {
+            if (response.ok) {
+                console.log('Mensaje de soporte enviado con éxito.');
+            } else {
+                console.error('Error al enviar el mensaje de soporte.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error en la solicitud: ', error);
+        });
+    }
+});
+
 
 function scrollToHash() {
     var hash = window.location.hash.substr(1);
