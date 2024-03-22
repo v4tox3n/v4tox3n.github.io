4069139fad7c2f6e875f47cddc5152ae9ed41539
adriiiaa12 <ap.poso1207@gmai.com>
2024-03-22 22:07:44 +0100

    Portfolio commit

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 var hamburguer = document.querySelector('.mobile-menu');
 var mobile_navbar = document.querySelector('.mobile-navbar');
 
+var headerButtons = document.querySelectorAll('.navbar a');
+
 hamburguer.addEventListener('click', function() {
     hamburguer.classList.toggle('fa-bars');
     hamburguer.classList.toggle('fa-x');
 
     mobile_navbar.classList.toggle('active');
+});
+
+headerButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        headerButtons.forEach(function(button){
+            button.classList.remove('active');
+        })
+        this.classList.toggle('active');
+    });
 });
