0682216e40b5db854db8c48a294957f4712ba3e1
adriiiaa12 <ap.poso1207@gmai.com>
2024-04-27 12:15:48 +0200

    Portfolio commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("Hi! Thanks for visiting my portfolio, but now it's not ready. I'm making it, so it's not finished. You can see my projects on my github -> adriiiaa12");
+
+menu = document.querySelector('.menu-links');
+
+function toggleMenu() {
+    // alert('ToggleMenu()');
+
+    menu.classList.toggle('active');
+}
+
