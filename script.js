ad4908b20969306deae37a8c27a724404a67f862
adriiiaa12 <ap.poso1207@gmai.com>
2024-05-11 13:40:41 +0200

    Portfolio commit

--- script.js
+++ script.js
@@ -26,6 +26,20 @@ function toggleMenu() {
 
 // RATING STAR SERVICES CONFIGURATION
 const allStars = document.querySelectorAll('.star');
+const number_stars = document.querySelector('.star_number');
+
+const star_rating_panel = document.querySelector('.star_rating');
+
+const next_button = document.querySelector('.next');
+const back_button = document.querySelector('.back');
+const send_button = document.querySelector('.send')
+
+const personal_comments_panel = document.querySelector('.personal-comments');
+const bad_valorations_panel = document.querySelector('.bad-valorations');
+const normal_valorations_panel = document.querySelector('.normal-valorations');
+const good_valorations_panel = document.querySelector('.good-valorations');
+
+let rating;
 
 allStars.forEach((star, i) => {
     star.onclick = function () {
@@ -39,5 +53,46 @@ allStars.forEach((star, i) => {
             }
 
         });
+
+        number_stars.innerHTML = current_star_rating + ' of 5';
+        rating = current_star_rating;
     }
 });
+
+function nextRatingPanel() {
+    if (rating === 0) {
+        alert('You have not selected any star');
+    } else {
+        if (rating === 1 || rating === 2) {
+            star_rating_panel.classList.add('active');
+            next_button.classList.add('active');
+            back_button.classList.add('active');
+            send_button.classList.add('active');
+            personal_comments_panel.classList.add('active');
+            bad_valorations_panel.classList.add('active');
+        } else if (rating === 3) {
+            star_rating_panel.classList.add('active');
+            next_button.classList.add('active');
+            back_button.classList.add('active');
+            send_button.classList.add('active');
+            personal_comments_panel.classList.add('active');
+            normal_valorations_panel.classList.add('active');
+        } else if (rating === 4 || rating === 5) {
+            star_rating_panel.classList.add('active');
+            next_button.classList.add('active');
+            back_button.classList.add('active');
+            send_button.classList.add('active');
+            personal_comments_panel.classList.add('active');
+            good_valorations_panel.classList.add('active');
+        }
+    }
+}
+
+function sendValoration() {
+    if (bad_valorations_panel.innerHTML === '' || normal_valorations_panel.innerHTML === '' || good_valorations_panel.innerHTML === '') {
+        alert('Please fill the gap with text.');
+    } else {
+        alert('Sent, thank you very much!');
+        window.location.href = 'https://adriiiaa12.github.io#services';
+    }
+}
